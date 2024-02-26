68a8a3a57c91b12a447b1d8f01e7eba273b4e70a
Tony Enerson <tony@enerson.ca>
2024-02-26 15:24:22 -0700

    Screen navigation

--- index.js
+++ index.js
@@ -9,12 +9,43 @@ const subTitles = [
     'A game formerly known as Farkle'
 ]
 
-const randomSubTitleIndex = Math.floor((Math.random()*subTitles.length))
-const randomSubTitle = subTitles[randomSubTitleIndex]
-document.getElementById("subTitle").innerHTML = randomSubTitle
+function changeSubtitle() {
+    const randomSubTitleIndex = Math.floor((Math.random()*subTitles.length))
+    const randomSubTitle = subTitles[randomSubTitleIndex]
+    document.getElementById("subTitle").innerHTML = randomSubTitle
+}
 
+function changeScreen(title, turnChange, playTurn, win) {
+    document.getElementById("titleScreen").style.display = title
+    document.getElementById("turnChangeScreen").style.display = turnChange
+    document.getElementById("playTurnScreen").style.display = playTurn
+    document.getElementById("winScreen").style.display = win
+}
 
-// show the screen I want to see
-document.getElementById("titleScreen").style.display = null
-document.getElementById("turnChangeScreen").style.display = 'none'
-document.getElementById("playTurnScreen").style.display = 'none'
+function showTitleScreen() {
+    changeSubtitle()
+    changeScreen(null, 'none', 'none', 'none')
+}
+
+function showTurnChange() {
+    changeScreen('none', null, 'none', 'none')
+}
+
+function showPlayTurn() {
+    changeScreen('none', 'none', null, 'none')
+}
+
+function showWin() {
+    changeScreen('none', 'none', 'none', null)
+}
+
+function startGame() {
+    // so something with the names ??
+    showTurnChange()
+}
+
+function startTurn() {
+    showPlayTurn()
+}
+
+showTitleScreen()
